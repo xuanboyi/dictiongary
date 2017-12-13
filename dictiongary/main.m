df1d49269f5f72664f7a2576e8d7e9238d55774d
玄博屹 <genhiroshi@xuanboyideMacBook-Pro.local>
2017-12-13 19:01:37 +0800

    1

--- dictiongary/main.m
+++ dictiongary/main.m
@@ -10,8 +10,33 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+       NSDictionary *dic1 = [[NSDictionary alloc]init];
+        NSDictionary *dic2 = [[NSDictionary alloc]initWithObjectsAndKeys:@"2.5",@"apple",@"2.2",@"orange", nil];
+        NSDictionary *dic3 = @{@"1.64":@"beijing",@"1.28":@"tianjin"};
+       // NSLog(@"%@ %@ %@",dic1,dic2,dic3);
+        NSString *str1 = [dic2 valueForKey:@"apple"];
+        NSLog(@"str1=%@",str1);
+        NSArray *array1 = [dic2 allKeys];
+        NSArray *array2 = [dic2 allValues];
+       // NSLog(@"%@ %@",array1,array2);
+        
+        for (int i = 0 ; i < array1.count; i++ ) {
+            NSString *item = [dic2 valueForKey:[array1 objectAtIndex:i]];
+           // NSLog(@"item=%@",item);
+        }
+        for (NSString *item2 in dic2){
+         //   NSLog(@"item2=%@",item2);
+        }
+        NSArray *array4=@[@"abc",@"123"];
+        NSDictionary *dic4 = [[NSDictionary alloc]initWithObjectsAndKeys:@"value1",@"keya",@"value2",@"key2",@"array4",@"keyd", nil];
+        
+        NSLog(@"dic4=%@",dic4);
+        for (int i =0; i < [dic4 allKeys].count; i++) {
+            id item1 = [dic4 valueForKey:[[dic4 allKeys]objectAtIndex:i]];
+            NSLog(@"item1=%@",item1);
+        }
+        
+        
     }
     return 0;
 }
